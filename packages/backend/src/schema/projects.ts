c62f235f7c6e14c09fcb6f4bde6d3f8f259f0ae8
Vedant Chainani <vedantchainani1084@gmail.com>
2025-10-04 03:22:55 +0530

    chore: update schema

--- packages/backend/src/schema/projects.ts
+++ packages/backend/src/schema/projects.ts
@@ -3,7 +3,34 @@ import { v } from "convex/values";
 
 export const projectsSchema = {
   projects: defineTable({
-    description: v.string(),
+    content: v.string(),
+    demoVideo: v.optional(v.string()),
+    images: v.object({
+      cover: v.string(),
+      icon: v.string(),
+      other: v.array(v.string()),
+    }),
+    liveDemo: v.optional(v.string()),
+    seo: v.object({
+      description: v.string(),
+      ogImage: v.string(),
+      title: v.string(),
+    }),
+    shortDescription: v.string(),
+    slug: v.string(),
+    sourceCode: v.optional(v.string()),
+    tags: v.array(v.string()),
     title: v.string(),
-  }),
+    tldr: v.optional(v.string()),
+  })
+    .index("by_slug", ["slug"])
+    .index("by_title", ["title"])
+    .index("by_tags", ["tags"])
+    .searchIndex("search_title", {
+      filterFields: ["tags"],
+      searchField: "title",
+    })
+    .searchIndex("search_content", {
+      searchField: "content",
+    }),
 };
